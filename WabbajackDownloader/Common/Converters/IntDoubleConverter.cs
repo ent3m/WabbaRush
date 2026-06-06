a009f35252189f4907dbef16f6cf5031509a4596
Entem <entemity@gmail.com>
2026-06-06 21:18:55 +0700

    Add integral numeric converters
    - Introduce IntDecimalConverter and IntDoubleConverter implementing IValueConverter for int/decimal and int/double conversions
    - Register both in Converters.axaml for use in XAML bindings

---
+++ WabbajackDownloader/Common/Converters/IntDoubleConverter.cs
@@ -0,0 +1,21 @@
+﻿using Avalonia.Data.Converters;
+using System.Globalization;
+
+namespace WabbajackDownloader.Common.Converters;
+
+internal sealed class IntDoubleConverter : IValueConverter
+{
+    public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        if (value is int intValue)
+            return (double)intValue;
+        return null;
+    }
+
+    public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        if (value is double decimalValue)
+            return (int)decimalValue;
+        return null;
+    }
+}
